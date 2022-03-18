8e5b0004851e287b2117b5ecf2212777b0038145
BinayRajbanshi <101475793+BinayRajbanshi@users.noreply.github.com>
2022-03-18 17:18:43 +0545

    corporate site first comit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Javascript for Glightbox
+const lightbox = GLightbox({ 
+    
+    
+    'href': 'https://youtu.be/gdsUKphmB3Y',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+    // 'autoplayVideos': 'true',
+    
+});
+// for aos
+AOS.init();
